cfc74dfa1b9525286c52362b1d7e66f8ea65dff2
Francisco Papini <francisco.papini@yahoo.com.ar>
2025-07-31 19:22:07 -0300

    Add files via upload

---
+++ vista/inc/foot.php
@@ -0,0 +1,111 @@
+<div id="loading">
+    	<div id="loading-img"></div>
+    	<p>Cargando</p>
+</div>
+
+    <div class="container">
+          <div style="text-align: center">
+                <img src="img/logo.png" alt="logo" width="200" height="200" style="float: none; margin: 0 auto;">
+          </div>
+    </div>
+
+<div class="container">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" style="margin:auto;">
+        <!-- Indicators -->
+            <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                  <li data-target="#myCarousel" data-slide-to="4"></li>
+            </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+              <div class="item active">
+                        <img class="center-block" src="img/corchero.jpg" alt="Corchero" style="height: 600px;">
+                  <div class="carousel-caption">
+                        <h3>Guarda Corchos</h3>
+                  </div>
+              </div>
+              <div class="item">
+                        <img class="center-block" src="img/barricaespecial.jpg" alt="Barrica" style="height: 600px;">
+                  <div class="carousel-caption">
+                        <h3>Barrig&oacute;n</h3>
+                  </div>
+              </div>
+              <div class="item">
+                        <img class="center-block" src="img/apoyacopas.jpg" alt="Apoya Copas" style="height: 600px">
+                  <div class="carousel-caption">
+                        <h3>Armario de Vinos</h3>
+                  </div>
+              </div>
+              <div class="item">
+                        <img class="center-block" src="img/frapera.jpg" alt="Frapera" style="height: 600px;">
+                  <div class="carousel-caption">
+                        <h3>Frapera</h3>
+                  </div>
+              </div>
+              <div class="item">
+                        <img class="center-block" src="img/masetero.jpg" alt="Masetero" style="height: 600px;">
+                  <div class="carousel-caption">
+                        <h3>Exteriores</h3>
+                  </div>
+              </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+
+    <div class="container">
+        <div class="jumbotron">
+                <h1 class="text-justify">Estimados Clientes</h1>
+                <p class="text-justify">Nos encontramos trabajando en nuestro nuevo sitio web. Pronto estar&aacute; terminado y podr&aacute;n ver nuestro cat&aacute;logo de productos</p>
+        </div>
+    </div>
+
+
+        <div class="container">
+                <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+                                <span class="sr-only">75% Complete</span>
+                        </div>
+                </div>
+        </div>
+
+<div class="navbar navbar-default">
+      <ul class="pager">
+        <li><h5>TE INVITAMOS A SEGUIRNOS EN NUESTRAS REDES</h5></li>
+      </ul>
+      <ul class="pager">
+        <li><a class="colorcito" href="https://api.whatsapp.com/send?phone=+543412024206&text=Hola,%20quisiera%20realizar%20una%20consulta!" target="_blank" style="background-color: #cfb084; border-color: #cfb084;"><img src="img/whatsapp.png" alt="whatsapp" width="32" height="32"></a></li>
+        <li><a class="colorcito" href="https://www.facebook.com/saboraroble" target="_blank" style="background-color: #cfb084; border-color: #cfb084;"><img src="img/facebook.png" alt="facebook" width="32" height="32"></a></li>
+        <li><a class="colorcito" href="https://www.instagram.com/saboraroble" target="_blank" style="background-color: #cfb084; border-color: #cfb084;"><img src="img/instagram.png" alt="instagram" width="32" height="32"></a></li>
+      </ul>
+      <ul class="pager">
+        <li><img src="img/telefono.png" alt="telefono" width="32" height="32"><a class="colorcito" href="tel:+543412024206" style="background-color: #cfb084; border-color: #cfb084; color: black;">341-2024206</a></li>
+        <li><img src="img/email.png" alt="email" width="32" height="32"><a id="colorcito" class="colorcito" href="mailto:saboraroble@gmail.com" target="_blank" style="background-color: #cfb084; border-color: #cfb084; color: black;">saboraroble@gmail.com</a></li>
+      </ul>
+</div><!-- /navbar -->
+
+
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-datepicker.js"></script>
+<script src="js/locales/bootstrap-datepicker.es.js"></script>
+<script src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap.js"></script>
+<script src="js/dataTables.bootstrap.js"></script>
+<script src="js/global.js"></script>
+</body>
+</html>
---
+++ vista/inc/head.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sabor A Roble</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/datepicker3.css" rel="stylesheet">
+    <link href="css/dataTables.bootstrap.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+<div class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <p><a href="index.php" class="navbar-brand">Sabor a Roble</a></p>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <!--
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Clientes <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="?modulo=cliente&accion=listar">Listar Clientes</a></li>
+                <li><a href="?modulo=cliente&accion=editar">Agregar Cliente</a></li>
+              </ul>
+            </li>
+             -->
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Productos <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="">Listar Productos</a></li>
+                <!--
+                <li><a href="?modulo=producto&accion=editar">Agregar Producto</a></li>
+                -->
+              </ul>
+            </li>
+          </ul>
+          <p class="navbar-text navbar-right"><strong>Bienvenido!</strong></p>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div><!-- /navbar -->
