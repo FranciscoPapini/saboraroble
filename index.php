2914aff556c9d1c2def2571268240f38265c1245
Francisco Papini <francisco.papini@yahoo.com.ar>
2025-07-31 19:21:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+error_reporting(E_ALL ^ E_NOTICE);
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+    require_once('vista/inc/head.php');
+    
+    require_once('vista/inc/foot.php');
+
+?>
